ca43b619b1b11d590e99639b60de0397fbe458d8
petterp <shiyihuicloud@163.com>
2020-11-11 23:36:16 +0800

    feat:添加剑指off-57-双指针解法

--- LiKou/src/和为s的两个数字.kt
+++ LiKou/src/和为s的两个数字.kt
@@ -18,4 +18,29 @@ class 和为s的两个数字 {
         }
         return intArrayOf()
     }
+
+
+    //此题的数组是递增关系，所以可以采用双指针方式
+    //如 nums = [10,26,30,31,47,60]，target=40
+    //理解 --right和 ++left 整个变换过程，不断调整整个数组适应大小变化
+    fun doubleNeedLe(nums: IntArray, target: Int): IntArray {
+        var left = 0
+        var right = nums.size - 1
+        while (left < right) {
+            val cur = nums[left] + nums[right]
+            when {
+                cur == target -> {
+                    return intArrayOf(nums[left], nums[right])
+                }
+                cur > target -> {
+                    --right
+                }
+                else -> {
+                    ++left
+                }
+            }
+        }
+        return intArrayOf()
+    }
+
 }
